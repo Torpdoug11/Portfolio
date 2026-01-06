9fedf11c6bd28626c19e1b1e4b39dc047471ad51
torpdoug11 <mokosaandrewelangwejr@gmail.com>
2026-01-06 15:57:33 +0100

    Fix Netlify build: import framer-motion motion in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { ThemeProvider } from './context/ThemeContext';
 import { DesignModeSelector } from './components/ui/DesignModeSelector';
 import Navbar from './components/Navbar';
